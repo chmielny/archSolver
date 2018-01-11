08d1cbf3c8eca643b8a9958affd957930605e469
chmielny <michal.chmielo@gmail.com>
2018-01-11 12:52:16 +0100

    solver
    -lepsze komentarze
    -zmiana nazwy modulu

--- solver.hs
+++ solver.hs
@@ -1,13 +1,26 @@
 -- typ danych okreslajacy kierunek przylacza zbiornika
--- z gazem
+-- z gazem, potrzebny na zewnatrz modulu, poniewaz
+-- wystepuje w funkcji solve
 -- lewo, prawo, gora, dol
 data GasDir = L | P | G | D deriving Show
 
 -- typ danych opisujacy drzewo rozwiazan
+-- typy wezlow/lisci:
+-- Empty - lisc rownowazny brakowi rozwiazania
+-- Solution - lisc zawierajacy rozwiazanie
+-- w formie listy pozycji zbiornikow
+-- z gazem oraz kierunkow ich przylaczy
+-- Node - wezel opisujacy czesciowe
+-- rozwiazanie, w kolejnych galeziach
+-- rozpatrywane sa kolejne pozycje
+-- zbiornikow zwiazanych z kolejnym
+-- domkiem na planszy
 data SolutionTree = Empty| Solution [(Int, Int, GasDir)] | Node SolutionTree SolutionTree SolutionTree SolutionTree deriving Show
 
 
 -- funkcja rozwiazujaca zagadke logiczna architekta
+-- powinna zostac wykorzystana jako jedyna na 
+-- zewnatrz modulu
 -- opis funkcji:
 -- ilosc zbior. w wierszach -> ilosc zbior. w kolumnach ->
 -- pozycje domkow --> pozycje zbior. z kier. przylacza
@@ -17,15 +30,25 @@ solve _ [] _ = []
 solve _ _ [] = []
 solve rowVals colVals housePos = findSolutionInTree (makeTree rowVals colVals housePos) 
 
---zamiana drzewa rozwiazan na rozwiazanie
+-- funkcja zamieniajaca drzewo rozwiazan na rozwiazanie
+-- opis funkcji:
+-- drzewo rozwiazan -> lista zbiornikow z gazem z 
+-- kierunkami przylaczy
 findSolutionInTree :: SolutionTree -> [(Int, Int, GasDir)]
 findSolutionInTree Empty = []
 findSolutionInTree (Solution list) = list
 findSolutionInTree (Node a b c d) = (findSolutionInTree a) ++ (findSolutionInTree b) ++ (findSolutionInTree c) ++ (findSolutionInTree d)
 
 
-
 -- funkcja tworzaca drzewo rozwiazan
+-- tworzy korzen drzewa z wykorzystaniem pomocniczej
+-- funkcji tworzącej drzewo makeTree' wywołanej 
+-- dla czterech sasiadujacych pol pierwszego domu 
+-- z listy
+-- opis funkcji:
+-- ilosc zbior. w wierszach -> ilosc zbior. w kolumnach ->
+-- pozycje domkow --> drzewo rozwiazan
+ 
 makeTree :: [Int] -> [Int] -> [(Int, Int)] -> SolutionTree
 makeTree [] _ _ = Empty
 makeTree _ [] _ = Empty
@@ -37,7 +60,21 @@ makeTree rowVals colVals ((houseY, houseX):houseRest) = Node
   (makeTree' (houseY, houseX - 1, P) houseRest rowVals colVals ((houseY, houseX):houseRest) [])
 
 
---                                   house                         const house list  actual gas tanks
+-- pomocnicza funkcja tworzaca drzewo rozwiazan
+-- wykorzystuje funkcje sprawdzajaca czy postawienie
+-- zbiornika na danym polu jest zgodne z zasadami gry
+-- (isLegal), jezeli tak to tworzy wezel oraz probuje
+-- utworzyc dalsze cztery zbiorniki na czterech polach
+-- sasiadujacych z kolejnym domem, jezeli postawienie
+-- zbiornika jest nielegalne to tworzy lisc Empty
+-- jezeli udalo sie wyczyscic wszystkie domki z listy
+-- to taki lisc uznajemy za rozwiazanie (Solution)
+-- opis funkcji
+-- badane pole wraz z przewidywanym kierunkiem przylacza ->
+-- lista domkow pozostalych do przylaczenia ->
+-- ilosc zbior. w wierszach -> ilosc zbior. w kolumnach ->
+-- pozycje wszystkich domkow --> aktualne pozycje zbiornikow ->
+-- drzewo rozwiazan
 makeTree' :: (Int, Int, GasDir) -> [(Int, Int)] -> [Int] -> [Int] -> [(Int, Int)] -> [(Int, Int, GasDir)] -> SolutionTree
 makeTree' (y, x, gasDir) [] rowVals colVals houseList gasTanks 
     |  isLegal (y, x) rowVals colVals houseList gasTanks == True  
@@ -48,8 +85,6 @@ makeTree' _ _ _ [] _ _ = Empty
 makeTree' _ _ _ _ [] _ = Empty
 makeTree' (y, x, gasDir) ((yh, xh):houseRest) rowVals colVals houseList gasTanks 
     | isLegal (y, x) rowVals colVals houseList gasTanks == True 
---      = Node [(y, x, gasDir)] 
---      = Node gasTanks 
       = Node 
       (makeTree' (yh - 1, xh, D) houseRest (decreaseListElem rowVals (y)) (decreaseListElem colVals (x)) houseList ((y, x, gasDir):gasTanks)) 
       (makeTree' (yh, xh + 1, L) houseRest (decreaseListElem rowVals (y)) (decreaseListElem colVals (x)) houseList ((y, x, gasDir):gasTanks)) 
@@ -59,6 +94,9 @@ makeTree' (y, x, gasDir) ((yh, xh):houseRest) rowVals colVals houseList gasTanks
 
 
 -- funkcja zmniejszajaca n-ty element listy o 1
+-- opis funkcji:
+-- lista -> pozycja elementu ->
+-- lista ze zdekrementowanym n-tym elementem
 decreaseListElem :: [Int] -> Int -> [Int]
 decreaseListElem [] _ = []
 decreaseListElem list n | n < 0 = []
@@ -109,7 +147,8 @@ isGasNeighbour (y, x) ((b, a, _):bas) | y < 0                    = True
 -- badane pole -> aktualne liczby zbiorników do 
 -- postawienia w wierszach -> aktualne liczby
 -- zbiornikow do postawienia w kolumnach ->
--- lista domow -> lista postawionych zbiornikow ->  wynik
+-- lista domow -> lista postawionych do tej pory zbiornikow 
+-- ->  wynik
 isLegal :: (Int, Int) -> [Int] -> [Int] -> [(Int, Int)] -> [(Int, Int, GasDir)] -> Bool
 isLegal _ [] _ _ _ = False
 isLegal _ _ [] _ _ = False
